09dca1bb19accf7b34ac6f8c03ea5f11fcac6a6f
Nandhakishore A <131781031+Nandhakishore23@users.noreply.github.com>
2026-01-02 12:18:05 +0530

    program2

---
+++ RotateArray.java
@@ -0,0 +1,48 @@
+
+import java.util.*;
+class RotateArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the array length:");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        System.out.println("Enter the array values one by one:");
+        for(int i = 0; i < n; i++){
+            nums[i] = sc.nextInt();
+        }
+        System.out.println("Enter the k value:");
+        int k = sc.nextInt();
+        nums = rotate(nums, k);
+        System.out.println("Output:")
+        for(int i = 0; i < n; i++){
+            System.out.print(nums[i] + " ");
+        }
+        sc.close();
+    }
+
+    public static int[] rotate(int[] nums, int k){
+        int n = nums.length;
+        k %= n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+        return nums;
+    }
+
+    public static void reverse(int[] nums, int start, int end){
+        while(start < end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
+
+    
+    
+    
+
+
+
